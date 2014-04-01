9720f8ea7a019a59296baa9aaceebbe91239b13f
oredi <darren_seet@oredi.com>
2014-04-02 00:07:21 +0900

    Complete Second Activity

--- GeoQuiz/GeoQuiz/src/main/java/me/seet/geoquiz/QuizActivity.java
+++ GeoQuiz/GeoQuiz/src/main/java/me/seet/geoquiz/QuizActivity.java
@@ -1,7 +1,9 @@
 package me.seet.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,10 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
         new TrueFalse(R.string.question_ocean, true),
@@ -24,10 +30,17 @@ public class QuizActivity extends ActionBarActivity {
         new TrueFalse(R.string.question_asia, true)
     };
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Called when activity is initially created, goes into Created state
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        if(savedInstanceState != null) {
+            // restore from bundle
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
+        }
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
@@ -68,6 +81,67 @@ public class QuizActivity extends ActionBarActivity {
                 getQuestion(-1);
             }
         });
+
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, mQuestionBank[mCurrentIndex].isTrueQuestion());
+                startActivityForResult(i, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        // Called before activity becomes visible from Created state or Stopped state (after onRestart)
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        // Called before activity becomes visible from Stopped state (before onRestart)
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        // Called before activity becomes active from Started State or Paused state
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        // Called before activity becomes inactive from Resumed state
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        // Called before activity becomes stopped from Paused state
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Called before activity becomes destroyed from Stopped state
+        super.onResume();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Called before activity onPause, onStop and onDestroy.
+        // Used to persist data across configuration changes
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState() called");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     @Override
@@ -90,7 +164,16 @@ public class QuizActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data == null)
+            return;
+
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+    }
+
     private void getQuestion(int offset) {
+        mIsCheater = false;
         mCurrentIndex += offset;                                           // Add offset + or - 1
         if(mCurrentIndex < 0) mCurrentIndex = mQuestionBank.length - 1;    // check lower boundary
         mCurrentIndex = mCurrentIndex % mQuestionBank.length;              // check upper boundary
@@ -100,10 +183,14 @@ public class QuizActivity extends ActionBarActivity {
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
         int messageResId = 0;
-        if(userPressedTrue == answerIsTrue)
-            messageResId = R.string.correct_toast;
-        else
-            messageResId = R.string.incorrect_toast;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
+        } else {
+            if(userPressedTrue == answerIsTrue)
+                messageResId = R.string.correct_toast;
+            else
+                messageResId = R.string.incorrect_toast;
+        }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
