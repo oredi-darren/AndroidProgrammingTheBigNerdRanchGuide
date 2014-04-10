e28f890b9ca17880349890dd326d1e10e45f43ae
Darren Seet Serkai <darren@seet.me>
2014-04-10 15:00:59 +0900

    Revert challenge

--- HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/AudioPlayer.java
+++ HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/AudioPlayer.java
@@ -3,46 +3,30 @@ package me.seet.hellomoon;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
-import android.view.SurfaceView;
 
 /**
  * Created by dseet on 4/9/2014.
  */
 public class AudioPlayer {
     private MediaPlayer mPlayer;
-    private int mCurrentPosition;
-    private SurfaceView mView;
-    public AudioPlayer(SurfaceView view) {
-        mView = view;
-    }
-    public void stop(boolean isResetPosition) {
-        if(isResetPosition) {
-            mCurrentPosition = 0;
-        }
+
+    public void stop() {
         if(mPlayer != null) {
-            mPlayer.release();
+            mPlayer.release();;
             mPlayer = null;
         }
     }
 
-    public void pause(){
-        mPlayer.pause();
-        mCurrentPosition = mPlayer.getCurrentPosition();
-    }
-
     public void play(Context c) {
-        stop(false);
+        stop();
 
-        //mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
-        mPlayer = MediaPlayer.create(c, R.raw.sample_mpeg4);
-        mPlayer.setDisplay(mView.getHolder());
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                stop(true);
+                stop();
             }
         });
-        mPlayer.seekTo(mCurrentPosition);
         mPlayer.start();
     }
 }
--- HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/HelloMoonFragment.java
+++ HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/HelloMoonFragment.java
@@ -1,10 +1,8 @@
 package me.seet.hellomoon;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,48 +13,32 @@ import android.widget.Button;
 public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
-    private Button mPauseButton;
-    private SurfaceView mSurfaceView;
-    private AudioPlayer mPlayer;
+    private AudioPlayer mPlayer = new AudioPlayer();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
-        mSurfaceView = (SurfaceView)v.findViewById(R.id.hellomoon_surfaceView);
-        mPlayer = new AudioPlayer(mSurfaceView);
+
         mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mPlayer.play(getActivity());
-                mPauseButton.setEnabled(true);
             }
         });
         mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
         mStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mPlayer.stop(true);
-                mPauseButton.setEnabled(false);
+                mPlayer.stop();
             }
         });
-
-        mPauseButton = (Button)v.findViewById(R.id.hellomoon_pauseButton);
-        mPauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mPlayer.pause();
-                mPauseButton.setEnabled(false);
-            }
-        });
-
-        mPauseButton.setEnabled(false);
         return v;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mPlayer.stop(true);
+        mPlayer.stop();
     }
 }
