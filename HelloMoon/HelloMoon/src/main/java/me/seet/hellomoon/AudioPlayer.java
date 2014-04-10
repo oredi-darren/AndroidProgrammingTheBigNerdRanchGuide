0e1a3305c648f443a16697bbcd4e2dcef425e3e8
Darren Seet Serkai <darren@seet.me>
2014-04-10 14:15:22 +0900

    Complete challenge to add a pause feature with video playback instead of sound

--- HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/AudioPlayer.java
+++ HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/AudioPlayer.java
@@ -3,30 +3,46 @@ package me.seet.hellomoon;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
+import android.view.SurfaceView;
 
 /**
  * Created by dseet on 4/9/2014.
  */
 public class AudioPlayer {
     private MediaPlayer mPlayer;
-
-    public void stop() {
+    private int mCurrentPosition;
+    private SurfaceView mView;
+    public AudioPlayer(SurfaceView view) {
+        mView = view;
+    }
+    public void stop(boolean isResetPosition) {
+        if(isResetPosition) {
+            mCurrentPosition = 0;
+        }
         if(mPlayer != null) {
-            mPlayer.release();;
+            mPlayer.release();
             mPlayer = null;
         }
     }
 
-    public void play(Context c) {
-        stop();
+    public void pause(){
+        mPlayer.pause();
+        mCurrentPosition = mPlayer.getCurrentPosition();
+    }
 
-        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+    public void play(Context c) {
+        stop(false);
+
+        //mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+        mPlayer = MediaPlayer.create(c, R.raw.sample_mpeg4);
+        mPlayer.setDisplay(mView.getHolder());
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                stop();
+                stop(true);
             }
         });
+        mPlayer.seekTo(mCurrentPosition);
         mPlayer.start();
     }
 }
--- HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/HelloMoonFragment.java
+++ HelloMoon/HelloMoon/src/main/java/me/seet/hellomoon/HelloMoonFragment.java
@@ -1,8 +1,10 @@
 package me.seet.hellomoon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -13,32 +15,48 @@ import android.widget.Button;
 public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
-    private AudioPlayer mPlayer = new AudioPlayer();
+    private Button mPauseButton;
+    private SurfaceView mSurfaceView;
+    private AudioPlayer mPlayer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
-
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.hellomoon_surfaceView);
+        mPlayer = new AudioPlayer(mSurfaceView);
         mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mPlayer.play(getActivity());
+                mPauseButton.setEnabled(true);
             }
         });
         mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
         mStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mPlayer.stop();
+                mPlayer.stop(true);
+                mPauseButton.setEnabled(false);
             }
         });
+
+        mPauseButton = (Button)v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.pause();
+                mPauseButton.setEnabled(false);
+            }
+        });
+
+        mPauseButton.setEnabled(false);
         return v;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mPlayer.stop();
+        mPlayer.stop(true);
     }
 }
